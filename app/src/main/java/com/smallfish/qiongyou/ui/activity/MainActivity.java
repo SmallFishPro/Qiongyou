05a6c35d56248ed9ad716cefd3aee3f5dbf74aaa
smallfish <1434488084@qq.com>
2016-03-14 15:07:27 +0800

    小修改

---
+++ app/src/main/java/com/smallfish/qiongyou/ui/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.smallfish.qiongyou.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.smallfish.qiongyou.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
